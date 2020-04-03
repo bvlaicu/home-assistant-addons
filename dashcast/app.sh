5e5e0b80c550f486b0232bd466dd231c569efe2e
Bogdan Vlaicu <bogdanvlaicu@yahoo.com>
2020-04-03 17:47:54 -0400

    Dashcast add-on

--- dashcast/app.sh
+++ dashcast/app.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+CONFIG_PATH=/data/options.json
+
+DEFAULT_DASHBOARD_URL=$(jq --raw-output '.dashboard_url // empty' $CONFIG_PATH)
+
+echo "DEFAULT_DASHBOARD_URL=${DEFAULT_DASHBOARD_URL}"
+
+export DEFAULT_DASHBOARD_URL
+
 # Keep the python app alive.
 while true; do python -u app_mqtt.py --show-debug; done
