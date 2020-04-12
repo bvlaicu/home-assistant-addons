9ab4a6691b28078b986ad62a1bbb65e728fff788
Bogdan Vlaicu <bogdanvlaicu@yahoo.com>
2020-04-12 12:56:29 -0400

    MiniDLNA add-on

--- minidlna/run.sh
+++ minidlna/run.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-> /etc/minidlna.conf
-
-for VAR in `env`; do
-    if [[ $VAR =~ ^MINIDLNA_ ]]; then
-        minidlna_name=`echo "$VAR" | sed -r "s/MINIDLNA_(.*)=.*/\1/g" | tr '[:upper:]' '[:lower:]'`
-        minidlna_value=`echo "$VAR" | sed -r "s/.*=(.*)/\1/g"`
-        echo "${minidlna_name}=${minidlna_value}" >> /etc/minidlna.conf
-    fi
-done
+#> /etc/minidlna.conf
+#
+#for VAR in `env`; do
+#    if [[ $VAR =~ ^MINIDLNA_ ]]; then
+#        minidlna_name=`echo "$VAR" | sed -r "s/MINIDLNA_(.*)=.*/\1/g" | tr '[:upper:]' '[:lower:]'`
+#        minidlna_value=`echo "$VAR" | sed -r "s/.*=(.*)/\1/g"`
+#        echo "${minidlna_name}=${minidlna_value}" >> /etc/minidlna.conf
+#    fi
+#done
 
 service minidlna force-reload
 
