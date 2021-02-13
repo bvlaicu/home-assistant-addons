1ea086676ffd0dc9d91ab5c6721458a793b289cb
Bogdan Vlaicu <bogdanvlaicu@yahoo.com>
2021-02-12 19:12:49 -0500

    oru logging improvements

--- oru/run.py
+++ oru/run.py
@@ -3,6 +3,7 @@ import asyncio
 from coned import Meter
 from coned import MeterError
 import paho.mqtt.client as mqtt
+import json
 
 meter = Meter(
     email=os.getenv("EMAIL"),
@@ -14,15 +15,18 @@ meter = Meter(
     site=os.getenv("SITE"),
     browser_path="/usr/bin/google-chrome-stable"
 )
-startTime, endTime, val, uom = asyncio.get_event_loop().run_until_complete(meter.last_read())
+startTime, endTime, value, uom = asyncio.get_event_loop().run_until_complete(meter.last_read())
 
-print(f"startTime: {startTime}, endTime: {endTime}, val: {val}, uom: {uom}")
+message = {'startTime': startTime, 'endTime': endTime, 'value': value, 'uom': uom}
+
+print(f"message: {message}")
 
 mqttc = mqtt.Client("oru_meter_reader")
 mqttc.username_pw_set(username=os.getenv("MQTT_USER"), password=os.getenv("MQTT_PASS"))
 mqttc.connect(os.getenv("MQTT_HOST"))
 mqttc.publish('electric_meter/startTime', startTime, retain=True)
 mqttc.publish('electric_meter/endTime', endTime, retain=True)
-mqttc.publish('electric_meter/value', val, retain=True)
+mqttc.publish('electric_meter/value', value, retain=True)
 mqttc.publish('electric_meter/uom', uom, retain=True)
+mqttc.publish('electric_meter/message', json.dumps(message), retain=True)
 mqttc.disconnect()
