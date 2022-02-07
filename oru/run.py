9681640d23820d7fa5a26ed9464acc726f9b49de
smitchsmith <github@mitchellzsmith.com>
2022-02-07 12:11:01 -0500

    Adds account number option

--- oru/run.py
+++ oru/run.py
@@ -12,6 +12,7 @@ meter = Meter(
     mfa_secret=os.getenv("MFA_SECRET"),
     account_uuid=os.getenv("ACCOUNT_UUID"),
     meter_number=os.getenv("METER_NUMBER"),
+    account_number=os.getenv("ACCOUNT_NUMBER"),
     site=os.getenv("SITE"),
     browser_path="/usr/bin/chromium-browser"
 )
